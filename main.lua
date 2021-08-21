ba5dd6d52bf343ca8749d448aac8477d400285c8
Jamesvictor12 <86720613+Jamesvictor12@users.noreply.github.com>
2021-08-21 13:02:59 -0300

    Add files via upload

---
+++ main.lua
@@ -0,0 +1,13 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+display.setDefault ("background" , 255, 255, 255)
+
+textObject = display.newText("Hello World, THE FUTURE IS NOW", 320, 240, native.systemFont)
+textObject:setTextColor (0,0,1)
+print("solar2dtest")
+
